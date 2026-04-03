bcc83a23474f52918c3545f35be21d90df06d300
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2026-04-03 09:28:13 +0200

    feat: add missing outputs (#25)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "config" {
   description = "contains all nat gateway configuration"
   value       = azurerm_nat_gateway.gw
 }
+
+output "subnet_associations" {
+  description = "contains all subnet nat gateway associations"
+  value       = azurerm_subnet_nat_gateway_association.gw_as
+}
+
+output "public_ip_associations" {
+  description = "contains all public ip nat gateway associations"
+  value       = azurerm_nat_gateway_public_ip_association.pip_as
+}
+
+output "public_ip_prefix_associations" {
+  description = "contains all public ip prefix nat gateway associations"
+  value       = azurerm_nat_gateway_public_ip_prefix_association.pippf_as
+}
